a9546a92cc32ef867e74cc066afa6e1e1b786cea
DiGureev <dianagureev@gmail.com>
2024-07-31 22:18:49 +0300

    feedback

---
+++ feedback.js
@@ -0,0 +1,4 @@
+// Nice project, it works and I love that your idea is very helpful these days. 
+// Good job! Only I wish you would also practise databases, knex, servers - all that thing. But since I know your curcumstances, I could say you did great! 
+
+// If you will want to improve it, you can use DB for storing questions and user answers and make login/logout :)
